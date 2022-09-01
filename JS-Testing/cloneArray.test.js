f58a7d34b931d4a7f6f1dc29ce2ce46e901361ee
Vipul Tanwar <vipultanwar45@gmail.com>
2022-09-01 14:11:59 +0530

    Testing Practice using JEST

---
+++ JS-Testing/cloneArray.test.js
@@ -0,0 +1,10 @@
+const { hasUncaughtExceptionCaptureCallback } = require("process");
+const cloneArray = require("./cloneArray");
+
+test('properly clones array', ()=> {
+    const array = [1,2,3]
+    expect(
+        cloneArray(array)
+    ).toEqual(array)
+    expect(cloneArray(array)).not.toBe(array)
+})
---
+++ JS-Testing/sum.test.js
@@ -0,0 +1,7 @@
+const sum = require('./sum');
+
+test('properly adds two number', () => {
+    expect(
+        sum(1,2)
+    ).toBe(3)
+})
